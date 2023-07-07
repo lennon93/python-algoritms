53a15d765a3962c44599562e907f8ada9f005de9
Lennon <lennon_lxs@hotmail.com>
2023-07-07 16:08:13 -0300

    termina o exercicio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    raise NotImplementedError
+    count = 0
+    if target_time == None:
+        return None
+    if not isinstance(permanence_period, list) or not all(isinstance(item, tuple) and all(isinstance(num, int) for num in item) for item in permanence_period):
+        return None
+    for item in permanence_period:
+        if item[0] <= target_time <= item[1]:
+            count += 1
+    return count
