cb1a26323462a50e988e967755de8c01e08098c4
Lennon <lennon_lxs@hotmail.com>
2023-07-07 17:16:28 -0300

    fix lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,10 @@
 def study_schedule(permanence_period, target_time):
     count = 0
-    if target_time == None:
+    if target_time is None:
         return None
-    if not isinstance(permanence_period, list) or not all(isinstance(item, tuple) and all(isinstance(num, int) for num in item) for item in permanence_period):
+    if not isinstance(permanence_period, list) or not all(
+            isinstance(i, tuple) and all(isinstance(n, int) for n in i)
+            for i in permanence_period):
         return None
     for item in permanence_period:
         if item[0] <= target_time <= item[1]:
