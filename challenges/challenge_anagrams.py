732c8b99dfc3b200bd3bf7018ecb14ba733e4771
Lennon <lennon_lxs@hotmail.com>
2023-07-08 01:49:32 -0300

    cria a funcao anagrama, mas falta diminuir a complexidade

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,26 @@
+def sort_string(string):
+    if string == '':
+        return ''
+
+    list_string = list(string.lower())
+    length = len(list_string)
+
+    for index in range(length):
+
+        for index_two in range(index + 1, length):
+            if list_string[index_two] < list_string[index]:
+
+                list_string[index] = list_string[index_two]
+                list_string[index_two] = list_string[index]
+
+    return ''.join(list_string)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first = sort_string(first_string)
+    second = sort_string(second_string)
+
+    if first != second or first == '' or second == '':
+        return (first, second, False)
+
+    return (first, second, True)
