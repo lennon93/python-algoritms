3c6acdc2c44946acc704a4ab7a85471c16332d43
Lennon <lennon_lxs@hotmail.com>
2023-07-07 21:56:44 -0300

    cria tests para a funcao encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('string', 13) == 'gnirts'
+
+    assert encrypt_message('string', 1) == 's_gnirt'
+
+    assert encrypt_message('string', 2) == 'gnir_ts'
+
+    assert encrypt_message('', 1) == ''
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('string', '1')
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 1)
