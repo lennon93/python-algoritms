12a6686b5c3a1d26afc2a39bb409a266380f9de6
Lennon <lennon_lxs@hotmail.com>
2023-07-10 16:30:03 -0300

    cria a função iterativa de palindromos

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    create_list = list(word.lower())[::-1]
+    reversed_word = "".join(create_list)
+
+    if word.lower() == reversed_word:
+        return True
+    return False
