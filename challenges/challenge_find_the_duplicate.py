a023bd7b69959ecf9e46c486e75c62e4ffa7073a
Lennon <lennon_lxs@hotmail.com>
2023-07-10 14:04:05 -0300

    Cria a função para ver se existe numeros repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums):   
+    if len(nums) < 2:
+        return False
+    
+    for i in nums:
+        if not isinstance(i, int):
+            return False
+        if i < 1:
+            return False
+        number = i
+        nums.pop(nums.index(i))    
+        if number in nums:
+            return number
+    return False
